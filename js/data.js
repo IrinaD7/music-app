309ddbfe20e79adc587513daca4fddbbc262d482
IrinaD7 <129327824+IrinaD7@users.noreply.github.com>
2023-04-13 21:22:58 +0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,213 @@
+
+let albums = [
+    {
+        title: `Для самых маленьких`,
+        img: `assets/child.jpg`,
+        year: `2010`,
+        description: `Жизнь детей без музыки любимых мультиков и фильмов скушна.`,
+        tracks: [
+            {
+                title: `Бюро находок`,
+                author: `Бюро находок`,
+                time: `01:35`,
+                src: `music/1.mp3`,
+                isPlaying : false
+            },
+            {
+                title: `Голубой вагон`,
+                author: `Крокодил Гена`,
+                time: `03:24`,
+                src: `music/2.mp3`,
+                isPlaying : false
+            },
+            {
+                title: `Крылатые качели`,
+                author: `Приключения Электроника`,
+                time: `03:14`,
+                src: `music/3.mp3`,
+                isPlaying : false
+            },
+            {
+                title: `Неприятность эту мы переживем`,
+                author: `Кот Леопольд`,
+                time: `01:31`,
+                src: `music/4.mp3`,
+                isPlaying : false
+            },
+            {
+                title: `Песня друзей`,
+                author: `Бременские музыканты`,
+                time: `02:09`,
+                src: `music/5.mp3`,
+                isPlaying : false
+            },
+            {
+                title: `Песня Чебурашки`,
+                author: `Чебурашка`,
+                time: `01:32`,
+                src: `music/6.mp3`,
+                isPlaying : false
+            },
+        ] 
+    },
+    
+    {
+        title: `Сделано в СССР`,
+        img: `assets/USSR.jpg`,
+        year: `1998`,
+        description: `Эти хиты гремели по стране совсем недавно. Но прошло полвека!`,
+        tracks: [
+            {
+                title: `Клён`,
+                author: `Синяя птица`,
+                time: `03:02`,
+                src: `music/7.mp3`,
+                isPlaying : false
+            },
+            {
+                title: `Все, что в жизни есть у меня`,
+                author: `Самоцветы`,
+                time: `03:51`,
+                src: `music/8.mp3`,
+                isPlaying : false
+            },
+            {
+                title: `Трава у дома`,
+                author: `Земляне`,
+                time: `04:45`,
+                src: `music/9.mp3`,
+                isPlaying : false
+            },
+            {
+                title: `Комарово`,
+                author: `Игорь Скляр`,
+                time: `02:12`,
+                src: `music/10.mp3`,
+                isPlaying : false
+            },
+            {
+                title: `Малиновка`,
+                author: `Верасы`,
+                time: `02:30`,
+                src: `music/11.mp3`,
+                isPlaying : false
+            },
+            {
+                title: `Птица счастья`,
+                author: `Николай Гнатюк`,
+                time: `02:34`,
+                src: `music/12.mp3`,
+                isPlaying : false
+            },
+        ] 
+    },
+
+    {
+        title: `Нестареющая классика`,
+        img: `assets/classic.jpg`,
+        year: `2007`,
+        description: `Музыкальные произведения, вошедшие в золотой фонд мировой культуры.`,
+        tracks: [
+            {
+                title: `Симфония №5`,
+                author: `Бетховен`,
+                time: `07:17`,
+                src: `music/13.mp3`,
+                isPlaying : false
+            },
+            {
+                title: `Токката и фуга ре минор`,
+                author: `Бах`,
+                time: `02:25`,
+                src: `music/14.mp3`,
+                isPlaying: false
+            },
+            {
+                title: `Полет валькирий`,
+                author: `Вагнер (Кольцо Нибелунга)`,
+                time: `03:06`,
+                src: `music/15.mp3`,
+                isPlaying: false
+            },
+            {
+                title: `Маскарад`,
+                author: `А.И. Хачатурян`,
+                time: `03:57`,
+                src: `music/16.mp3`,
+                isPlaying: false
+            },
+            {
+                title: `Лунная Соната`,
+                author: `Бетховен `,
+                time: `05:44`,
+                src: `music/17.mp3`,
+                isPlaying: false
+            },
+            {
+                title: `Вальс Цветов`,
+                author: `Чайковский`,
+                time: `07:09`,
+                src: `music/18.mp3`,
+                isPlaying: false
+            },
+            {
+                title: `К Элизе`,
+                author: `Бетховен`,
+                time: `02:38`,
+                src: `music/111.mp3`,
+                isPlaying : false
+            }
+        ] 
+    },
+
+    {
+        title: `Великие хиты шансона`,
+        img: `assets/chanson.jpg`,
+        year: `2013`,
+        description: `Люди любят шансон за понятность и искренность.`,
+        tracks: [
+            {
+                title: `Владимирский централ`,
+                author: `Михаил Круг`,
+                time: `04:24`,
+                src: `music/19.mp3`,
+                isPlaying: false
+            },
+            {
+                title: `В таверне`,
+                author: `Иван Кучин `,
+                time: `04:20`,
+                src: `music/20.mp3`,
+                isPlaying: false
+            },
+            {
+                title: `Третье сентября`,
+                author: `Михаил Шуфутинский`,
+                time: `06:18`,
+                src: `music/21.mp3`,
+                isPlaying: false
+            },
+            {
+                title: `Поцелуй Меня, Удача!`,
+                author: `Анатолий Полотно & Федя Карманов`,
+                time: `03:50`,
+                src: `music/22.mp3`,
+                isPlaying: false
+            },
+            {
+                title: `Запахло Весной`,
+                author: `Бутырка`,
+                time: `04:10`,
+                src: `music/23.mp3`,
+                isPlaying: false
+            },
+            {
+                title: `Снегири`,
+                author: `Сергей Трофимов`,
+                time: `03:00`,
+                src: `music/24.mp3`,
+                isPlaying: false
+            },
+        ] 
+    },
+]
